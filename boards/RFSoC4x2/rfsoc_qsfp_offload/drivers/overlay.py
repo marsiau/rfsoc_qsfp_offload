e2e75b60d9a5a192426b85abcea074fdd0ea306c
marsiau <marius.siauciulis@gmail.com>
2026-01-27 14:34:46 +0000

    N2 update

--- boards/RFSoC4x2/rfsoc_qsfp_offload/drivers/overlay.py
+++ boards/RFSoC4x2/rfsoc_qsfp_offload/drivers/overlay.py
@@ -83,7 +83,12 @@ class Overlay (Overlay):
     def set_fc(self, tile, block, fc):
         """ Change the center frequency.
         """
-        self.rfdc.adc_tiles[tile].blocks[block].MixerSettings['Freq'] = -fc
+        if fc <= 4915200:
+            self.rfdc.adc_tiles[tile].blocks[block].MixerSettings['Freq'] = -fc
+            self.rfdc.adc_tiles[tile].blocks[block].NyquistZone = 1
+        else
+            self.rfdc.adc_tiles[tile].blocks[block].MixerSettings['Freq'] = fc
+            self.rfdc.adc_tiles[tile].blocks[block].NyquistZone = 2
         self.rfdc.adc_tiles[tile].blocks[block].UpdateEvent(xrfdc.EVENT_MIXER)
         return fc
 
